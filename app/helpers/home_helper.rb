b8f6b3563f5a522ddb7b181e4bd230206414738c
Ken Lu <ken@seeice.com>
2011-11-06 22:42:41 -0500

    remove the dragable item list as a partial

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,18 @@
 module HomeHelper
+  def priority2color color
+    case color
+    when 1
+      'color-orange'
+    when 2
+      'color-green'
+    when 3
+      'color-red'
+    when 4
+      'color-yellow'
+    when 5
+      'color-blue'
+    else
+      'color-white'
+    end
+  end
 end
